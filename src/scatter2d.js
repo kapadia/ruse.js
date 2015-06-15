bd4fc11eb42976fd3cd935b542315c991c987d0b
Amit Kapadia <amit@planet.com>
2015-06-15 11:49:28 -0700

    update scatter 2d to accept typed array

--- src/scatter2d.js
+++ src/scatter2d.js
@@ -1,7 +1,7 @@
-
+// Data should be a Float32Array containing x/y values serialized as x0, y0, x1, y1, etc.
 ruse.prototype.scatter2D = function(data) {
-  var datum, i, index, initialVertices, margin, max1, max2, min1, min2, nVertices, range1, range2, vertexSize, vertices, _i, _j, _len, _len1, _ref, _ref1, _ref2, _ref3;
-  
+  var datum, i, index, initialVertices, margin, max1, max2, min1, min2, nVertices, range1, range2, vertexSize, _i, _j, _len, _len1, _ref, _ref1, _ref2, _ref3;
+
   this.gl.useProgram(this.programs.ruse);
   if (this.state !== "scatter2D") {
     this["switch"] = 0;
@@ -21,28 +21,17 @@ ruse.prototype.scatter2D = function(data) {
   vertexSize = 2;
   nVertices = data.length;
   
-  vertices = new Float32Array(vertexSize * nVertices);
-  
   _ref = Object.keys(data[0]), this.key1 = _ref[0], this.key2 = _ref[1];
   _ref1 = this.getExtentFromObjects(data), (_ref2 = _ref1[0], min1 = _ref2[0], min2 = _ref2[1]), (_ref3 = _ref1[1], max1 = _ref3[0], max2 = _ref3[1]);
   
   range1 = max1 - min1;
   range2 = max2 - min2;
   
-  for (index = _i = 0, _len = data.length; _i < _len; index = ++_i) {
-    datum = data[index];
-    i = vertexSize * index;
-    vertices[i] = datum[this.key1];
-    vertices[i + 1] = datum[this.key2];
-  }
   if (!this.hasData) {
-    initialVertices = new Float32Array(vertexSize * nVertices);
-    for (index = _j = 0, _len1 = data.length; _j < _len1; index = ++_j) {
-      datum = data[index];
-      i = vertexSize * index;
-      initialVertices[i] = datum[this.key1];
-      initialVertices[i + 1] = min2;
-    }
+    
+    // Another set of x-y values are stored here, used for transition from one
+    // dataset to another.
+    initialVertices = new Float32Array(data);
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.dataBuffer1);
     this.gl.bufferData(this.gl.ARRAY_BUFFER, initialVertices, this.gl.STATIC_DRAW);
     this.extents = {
@@ -66,12 +55,12 @@ ruse.prototype.scatter2D = function(data) {
     this.gl.uniform3f(this.uMinimum2, min1, min2, 0);
     this.gl.uniform3f(this.uMaximum2, max1, max2, 1);
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.dataBuffer2);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, this.gl.STATIC_DRAW);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, data, this.gl.STATIC_DRAW);
     this.gl.vertexAttribPointer(this.programs.ruse.aVertexPosition2, this.dataBuffer2.itemSize, this.gl.FLOAT, false, 0, 0);
     this["switch"] = 1;
   } else {
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.dataBuffer1);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, this.gl.STATIC_DRAW);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, data, this.gl.STATIC_DRAW);
     this.gl.vertexAttribPointer(this.programs.ruse.aVertexPosition1, this.dataBuffer1.itemSize, this.gl.FLOAT, false, 0, 0);
     this.gl.uniform3f(this.uMinimum1, min1, min2, 0);
     this.gl.uniform3f(this.uMaximum1, max1, max2, 1);
