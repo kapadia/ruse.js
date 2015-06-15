76c5d079193aa4d75d4a6474ecebcf053d5390be
Amit Kapadia <amit@planet.com>
2015-06-15 11:48:56 -0700

    update githubs raw content url

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ rm examples/data/*
 mkdir -vp examples/lib
 mkdir -vp examples/data
 
-curl -k "https://raw.github.com/astrojs/fitsjs/rewrite/lib/fits.js" -o 'examples/lib/fits.js'
-curl -k "https://raw.github.com/toji/gl-matrix/master/dist/gl-matrix.js" -o 'examples/lib/gl-matrix.js'
+curl -k "https://raw.githubusercontent.com/astrojs/fitsjs/master/lib/fits.js" -o 'examples/lib/fits.js'
+curl -k "https://raw.githubusercontent.com/toji/gl-matrix/master/dist/gl-matrix-min.js" -o 'examples/lib/gl-matrix.js'
 curl -k "http://astrojs.s3.amazonaws.com/sample/plates-dr9.fits" -o 'examples/data/plates-dr9.fits'
